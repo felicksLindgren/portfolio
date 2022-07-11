85eb67d1d33b2836651cd0dfec4d377daea018f7
Felix Lindgren <felix.p.lindgren@gmail.com>
2022-07-11 20:41:18 +0200

    fixing pwa config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
-const runtimeCache = require('next-pwa/cache');
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    runtimeCache
+    runtimeCaching
   },
   output: 'standalone',
 });
